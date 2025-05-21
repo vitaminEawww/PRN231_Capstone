b8e10609e462f7bd1cf0867d5176a38f14ed8e90
NET_13_LeXuanPhuongNam <namlxpse173365@fpt.edu.vn>
2025-05-21 23:16:11 +0700

    UnitofWork

---
+++ PRN231_BE/Repositories/Repository/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+using Repositories.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly ApplicationDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public GenericRepository( ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);   
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+    }
+}
---
+++ PRN231_BE/Repositories/Repository/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using DataAccess.Data;
+using Repositories.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Repository
+{
+    internal class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UnitOfWork(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+
+        }
+
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _dbContext.SaveChangesAsync();
+        }
+    }
+}
